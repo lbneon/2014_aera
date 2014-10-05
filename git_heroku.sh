4b84ee5f29f6b3f5bd07a279da0349ce95dd16e5
lbneon <lbneon@gmail.com>
2014-10-05 11:20:18 +0000

    release beta

--- git_heroku.sh
+++ git_heroku.sh
@@ -1,5 +1,7 @@
 git add .
 git commit -m "release beta" -a
 git push origin master -f
+git checkout gh-pages
+git commit -m "release beta" -a
 git push origin gh-pages -f
 git push bitbucket master -f
